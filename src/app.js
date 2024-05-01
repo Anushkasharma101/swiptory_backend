a71c5c68cd615b0e809582039aa79733987b1ad9
Anushka <as0846403@gmail.com>
2024-05-01 06:26:45 +0530

    port changed

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const verifyToken = require('./middleware/verifyAccessToken');
 require("dotenv").config();
 const app = express();
-const PORT = process.env.PORT || 2000;
+const PORT = process.env.PORT||2000;
 require('./service/conn');
 app.use(express.json());
 
